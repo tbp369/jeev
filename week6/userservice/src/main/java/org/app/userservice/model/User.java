7c6fbd973026804d30e4e6e00c6ecfefb5f3a494
tbp369 <74483910+tbp369@users.noreply.github.com>
2025-07-30 10:30:04 +0530

    Add files via upload

---
+++ week6/userservice/src/main/java/org/app/userservice/model/User.java
@@ -0,0 +1,31 @@
+package org.app.userservice.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a user in the system.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+}
